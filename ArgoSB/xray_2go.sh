a15302cf47d0dc324fdfbc65ecbc4adce2fe8520
wszx123 <108945398+wszx123@users.noreply.github.com>
2026-05-18 20:08:25 +0800

    Update xray_2go.sh

--- ArgoSB/xray_2go.sh
+++ ArgoSB/xray_2go.sh
@@ -1068,7 +1068,7 @@ while true; do
    clear
    echo ""
    green "===== Xray-2go一键安装脚本 =====\n"
-   blue "----- ws01 v20260518 -----\n"
+   green "----- ws01 v20260518 -----\n"
    red " Xray 状态: ${check_xray_status}\n"
    red " Argo 状态: ${check_argo_status}\n"   
    red "Caddy 状态: ${check_caddy_status}\n"
